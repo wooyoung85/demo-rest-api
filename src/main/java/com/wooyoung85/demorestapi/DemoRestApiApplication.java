01a37f6955f14e320ba59522f2e04fc59c2cbaea
wooyoung85 <cerealtigerpower@gmail.com>
2019-11-28 17:15:35 +0900

    first commit

---
+++ src/main/java/com/wooyoung85/demorestapi/DemoRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.wooyoung85.demorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wooyoung85/demorestapi/DemoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooyoung85.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
