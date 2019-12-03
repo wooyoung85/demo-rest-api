b0237e74c21f4a90df5730597beb155b01850b31
wooyoung85 <cerealtigerpower@gmail.com>
2019-12-03 18:00:36 +0900

    add test desciption

--- src/test/java/com/wooyoung85/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/wooyoung85/demorestapi/events/EventControllerTest.java
@@ -1,6 +1,7 @@
 package com.wooyoung85.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wooyoung85.demorestapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +33,7 @@ public class EventControllerTest {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -62,6 +64,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -87,6 +90,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -99,6 +103,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
